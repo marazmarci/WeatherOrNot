9bf5d66fbb78d845e42793317a3cf4cb979da4ff
Márton Maráz <marazmarci@gmail.com>
2021-01-21 21:44:12 +0100

    Add initial Android project

---
+++ app/src/main/java/dev/maraz/weatherornot/MainActivity.kt
@@ -0,0 +1,18 @@
+package dev.maraz.weatherornot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.maraz.weatherornot.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
